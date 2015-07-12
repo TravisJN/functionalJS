969b5e456c91b2243d089b47f27a78fa907515b6
travisjn <tdizzletrav@gmail.com>
2015-07-11 21:19:00 -0700

    Solutions through callbacks

--- callbacks/callbacks.js
+++ callbacks/callbacks.js
@@ -1,1 +1,36 @@
 // Complete your callbacks exercises here
+var funcCaller = function(func, arg) {
+  return func(arg);
+};
+
+var firstVal = function(arr, func) {
+  if (Array.isArray(arr) === false) {
+    for(var key in arr) {
+      func(arr[key], key, arr);
+      break;
+    }
+  } else {
+    func(arr[0], 0, arr)
+  }
+};
+
+var once = function(func) {
+  var alreadyCalled = false;
+  var result;
+
+  return function() {
+    if (!alreadyCalled) {
+      result = func();
+      alreadyCalled = false;
+    }
+
+    return result;
+  };
+}
+
+var chargeCreditCard = function(num, price) {
+
+};
+
+var processPaymentOnce = once(chargeCreditCard);
+processPaymentOnce(12323, 200);
--- closures/closures.js
+++ closures/closures.js
@@ -1,1 +1,115 @@
 // Complete your closures exercises here
+var nonsense = function(string) {
+  var blab = function() {
+    alert(string);
+  };
+  return blab;
+};
+
+var blabLater = nonsense('hello');
+var blabAgainLater = nonsense('goodbye');
+
+var lastNameTrier = function(firstName) {
+  var innerFunction = function(lastName) {
+    return firstName + lastName;
+  };
+
+  return innerFunction;
+};
+
+var firstNameFarmer = lastNameTrier('Farmer');
+firstNameFarmer('Brown');
+
+var storyWriter = function() {
+  var story = '';
+
+  return {
+    addWords: function(word) {
+      story += word;
+      return story;
+    },
+    erase: function() {
+      story = '';
+    }
+  };
+};
+
+var Toaster = function() {
+  var state = 0; // off
+
+  var offOn = function() {
+    state = !state;
+  }
+
+  return {
+    toast: function(food) {
+      return 'Your ' + food + ' is now toasty';
+    },
+    turnOn: function() {
+      if (state) {
+        return 'The toaster is already on!';
+      } else {
+        switchState();
+        return 'The toaster is now on';
+      }
+    },
+    turnOff: function() {
+      if (state) {
+        switchState();
+        return 'The toaster is now off';
+      } else {
+        return 'The toaster is already off!';
+      }
+    }
+  }
+};
+
+var checkAttendanceFunc = function(nameArr){
+    var resultArr = [];
+    for(var i = 0; i < nameArr.length; i++){
+      (function(idx){
+        resultArr.push(function(){ console.log('Is', nameArr[idx], 'present?', idx)})
+      })(i);
+    };
+    return resultArr;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
